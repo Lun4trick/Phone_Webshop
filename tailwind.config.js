372320a034411a94ad54d0495b8cb55a564ff7a1
Levente Krotos <krotos.levi@gmail.com>
2023-03-09 16:49:26 +0100

    adbar finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'mont': ['mont']
+    },
     screens: {
       'mobile': '0px',
       'tablet': '640px',
       'laptop': '1024px',
       'desktop': '1280px',
     },
+    extend: {
+      backgroundImage: {
+        'iphone-14-back': "url('./imgs/adScrollImgs/iphone-14-back.png')",
+        'iphone-14-pros': "url('./imgs/adScrollImgs/iphone-14-pros.png')",
+        'airpods': "url('./imgs/adScrollImgs/airpods.png')",
+        'ipad-pro': "url('./imgs/adScrollImgs/ipad-pro.png')",
+      }
+    }
   },
   plugins: [],
 }
