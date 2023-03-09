6e5be940884770b4126edde53af088fb14429d5f
Levente Krotos <krotos.levi@gmail.com>
2023-03-09 18:21:05 +0100

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Footer } from './Components/Footer';
 import { Header } from "./Components/Header"
 import { HomePage } from './Components/HomePage';
 
@@ -8,6 +9,10 @@ function App() {
     <main className="min-h-screen bg-[#0F1121]">
       <Header />
       <HomePage />
+      <div className='h-[1000px]'>
+        fdsaf
+      </div>
+      <Footer/>
     </main>
   );
 }
