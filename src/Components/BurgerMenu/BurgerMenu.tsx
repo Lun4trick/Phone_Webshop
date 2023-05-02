e2d7adbe4fdf9a1256b2f71cd126982f80a3b051
Levente Krotos <krotos.levi@gmail.com>
2023-05-02 15:56:21 +0200

    burger menu/sohpping cart and favourites added

--- src/Components/BurgerMenu/BurgerMenu.tsx
+++ src/Components/BurgerMenu/BurgerMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import NavItems from '../../utils/map/NavItems';
+import { useAppSelector } from '../../app/hooks';
 
 type Props = {
   isMenuOpen: boolean;
@@ -10,6 +11,8 @@ export const BurgerMenu: React.FC<Props> = ({ isMenuOpen }) => {
   const headerHeight = document.getElementById('header')?.offsetHeight ?? 0;
   const viewPortHeight = window.innerHeight;
   const menuHeight = (viewPortHeight - (headerHeight));
+  const cartLength = useAppSelector(state => state.cartItems).cartItems.length;
+  const favouritesLength = useAppSelector(state => state.favouriteItems).favouriteItems.length;
 
   return (
     <div
@@ -37,21 +40,45 @@ export const BurgerMenu: React.FC<Props> = ({ isMenuOpen }) => {
           </NavLink>
         ))}
       </ul>
-      <div className='tablet:hidden w-full border-t border-Surface-2 h-[64px]'>
-        <button type='button' className='w-1/2 p-4 h-full laptop:p-5 items-center border-Surface-2 hover:bg-slate-600 active:bg-slate-500 active:border-b-4 active:border-b-white'>
-          <img
-            className='mx-auto'
-            src={`${process.env.PUBLIC_URL!}/imgs/favourites_icon.svg`}
-            alt=''
-          />
-        </button>
-        <button type='button' className='w-1/2 p-4 h-full justify-center laptop:p-5  border-l border-Surface-2 items-center hover:bg-slate-600 active:bg-slate-500 active:border-b-4 active:border-b-white'>
-          <img
-            src={`${process.env.PUBLIC_URL!}/imgs/shopping_cart.svg`}
-            className='mx-auto'
-            alt=''
-          />
-        </button>
+      <div className='tablet:hidden flex w-full border-t border-Surface-2 h-[64px]'>
+        <NavLink
+          to='/favourites'
+          type='button'
+          className='h-full items-center justify-center w-1/2 flex m-0 laptop:p-5 border-Surface-2 hover:bg-slate-600 active:bg-slate-500 active:border-b-4 active:border-b-white'
+        >
+          <div className='relative'>
+            {favouritesLength > 0 && (
+              <div className=' flex justify-center items-center w-[14px] h-[14px] bg-Phone-Red rounded-full text-[9px] absolute bottom-[50%] left-[50%] z-60'>
+                <p className='flex justify-center items-end w-[14px] h-[14px] text-Phone-white'>
+                  {favouritesLength}
+                </p>
+              </div>
+            )}
+            <img
+              src={`${process.env.PUBLIC_URL!}/imgs/favourites_icon.svg`}
+              alt='favourites'
+            />
+          </div>
+        </NavLink>
+        <NavLink
+          to='/shoppingCart'
+          type='button'
+          className='flex items-center justify-center w-1/2 h-full laptop:p-5 border-l border-Surface-2 hover:bg-slate-600 active:bg-slate-500 active:border-b-4 active:border-b-white'
+        >
+          <div className='relative'>
+            {cartLength > 0 && (
+              <div className=' flex justify-center items-center w-[14px] h-[14px] bg-Phone-Red rounded-full text-[9px] absolute bottom-[50%] left-[50%] z-60'>
+                <p className='flex justify-center items-end w-[14px] h-[14px] text-Phone-white'>
+                  {cartLength}
+                </p>
+              </div>
+            )}
+            <img
+              src={`${process.env.PUBLIC_URL!}/imgs/shopping_cart.svg`}
+              alt='shoppingCart'
+            />
+          </div>
+        </NavLink>
       </div>
     </div>
   );
